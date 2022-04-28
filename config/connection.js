255ddd72540ee9b9e34c6f7bf925c13564e20d84
saba mahd <sabamahd.sm@gmail.com>
2022-04-28 19:44:03 -0400

    connection file made

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+// import sequelize
+const dbConfig = require("../config/db.config.js");
+
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+     sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+        host: dbConfig.HOST,
+        dialect: dbConfig.dialect,
+        operatorsAliases: false,
+        pool: {
+          max: dbConfig.pool.max,
+          min: dbConfig.pool.min,
+          acquire: dbConfig.pool.acquire,
+          idle: dbConfig.pool.idle
+        }
+      });
+}
+
+module.exports = sequelize;
