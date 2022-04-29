a97ad7ab1485ccde5382f23c7a1f6530ef79e40f
saba mahd <sabamahd.sm@gmail.com>
2022-04-29 16:04:06 -0400

    fixed img source added menu handlebars

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    HOST: "127.0.0.1",
+    HOST: "localhost",
     USER: "root",
-    PASSWORD: "123456",
-    DB: "testdb",
+    PASSWORD: "Siena2021!",
+    DB: "LettucB-Frank-Project",
     dialect: "mysql",
     pool: {
-      max: 5,
+      max: 10,
       min: 0,
       acquire: 30000,
       idle: 10000
